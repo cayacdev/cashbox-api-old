1b829ec9b32cead1586e45128a3d96f88e62c633
Philipp Müller <pmueller.wf@gmail.com>
2020-08-15 12:36:32 +0200

    fix some camel case issues

--- database/migrations/2020_08_01_104021_create_cash_boxes_table.php
+++ database/migrations/2020_08_01_104021_create_cash_boxes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCashboxesTable extends Migration
+class CreateCashBoxesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -28,6 +28,6 @@ class CreateCashboxesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cashboxes');
+        Schema::dropIfExists('cash_boxes');
     }
 }
--- database/migrations/2020_08_01_104330_create_cash_box_budget_plans.php
+++ database/migrations/2020_08_01_104330_create_cash_box_budget_plans.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCashboxBudgetPlans extends Migration
+class CreateCashBoxBudgetPlans extends Migration
 {
     /**
      * Run the migrations.
@@ -33,6 +33,6 @@ class CreateCashboxBudgetPlans extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cashbox_budget_plans');
+        Schema::dropIfExists('cash_box_budget_plans');
     }
 }
